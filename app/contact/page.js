b505a4be5b9ade7f60b8587e5087a8551646751a
James de Jesus <jamesdejesus@Jamess-MacBook-Air.local>
2026-05-30 07:59:35 -0700

    add honeypot

--- app/contact/page.js
+++ app/contact/page.js
@@ -12,6 +12,7 @@ export default function Contact() {
         <form name="contact" action="https://api.web3forms.com/submit" method="POST">
           <input type="hidden" name="access_key" value={process.env.NEXT_PUBLIC_WEB3FORMS_KEY} />
           <input type="hidden" name="redirect" value={`${process.env.NEXT_PUBLIC_SITE_URL}/success`} />
+          <input type="checkbox" name="botcheck" style={{ display: 'none' }} />
           <div className="row mb-3">
             <div className="col-md-2"><label htmlFor="name">Your Name:</label></div>
             <div className="col"><input type="text" name="name" className="form-control" /></div>
